df44ba52993e3c5cac0ffcf3188e371aa13c0bba
E Smith <brs9947@rit.edu>
2021-02-10 17:48:21 -0500

    created runner minigame (demo version)
    bare-bones playable demo

---
+++ KiKi's Game/Assets/RunnerGame/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public const int MAX_HP = 100;
+    public int currentHP = 100;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = MAX_HP;
+        healthBar.SetMaxHealth(MAX_HP);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        healthBar.SetHealth(currentHP);
+    }
+}
