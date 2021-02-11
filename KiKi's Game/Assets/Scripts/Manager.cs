6d528cb5a7fb53ee133c2fec73ec65811dbbbdb6
Cassy <cassy.s.snyder@gmail.com>
2021-02-11 11:46:21 -0500

    Flying game
    This should only upload the information to the cassy branch

---
+++ KiKi's Game/Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Manager : MonoBehaviour
+{
+    public Player kiki;
+    public GameObject broom;
+    public GameObject background;
+    public GameObject bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kiki = new Player();
+        kiki.health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void CheckCollide()
+    {
+
+    }
+}
