8d3ad9cbbd826574f2ed373d786f90432494bc8e
cassy_laptop <cassy.s.snyder@gmail.com>
2021-02-12 14:17:18 -0500

    Fix Scripts
    flying game

---
+++ KiKi's Game/Assets/Scripts/FlyerScripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public Manager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            manager.isPaused = true;
+            pauseScreen.SetActive(true);
+        }
+    }
+
+    void UnpauseButton()
+    {
+        manager.isPaused = false;
+        pauseScreen.SetActive(false);
+    }
+}
