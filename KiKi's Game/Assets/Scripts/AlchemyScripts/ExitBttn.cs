ac1e2e4081000e6b0be859a586eec521059ab519
niantoniadis <naa3207@rit.edu>
2021-02-12 20:46:38 -0500

    First Playable

--- KiKi's Game/Assets/Scripts/AlchemyScripts/ExitBttn.cs
+++ KiKi's Game/Assets/Scripts/AlchemyScripts/ExitBttn.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitBttn : MonoBehaviour
 {
     public void QuitGame()
     {
         // save any game data here
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         // Application.Quit() does not work in the editor so
         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-        UnityEditor.EditorApplication.isPlaying = false;
+        SceneManager.LoadScene(0);
         #else
          Application.Quit();
         #endif
