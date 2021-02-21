54888d559326f8283245990152fe37207a5b28c3
E Smith <brs9947@rit.edu>
2021-02-20 23:10:30 -0500

    created new HelpButton prefab
    prefab is only implemented in Runner game at the moment. TODO: make jiji pause the game

---
+++ KiKi's Game/Assets/Scripts/HelpBtn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpBtn : MonoBehaviour
+{
+    public GameObject jijiinfo;
+
+
+    public void Click()
+    {
+        jijiinfo.SetActive(true);
+    }
+
+    public void ExitBtnClick()
+    {
+        jijiinfo.SetActive(false);
+    }
+}
