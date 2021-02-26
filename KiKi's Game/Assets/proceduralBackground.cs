206444ac09d39241bf2501725e6e6de4db595d1c
niantoniadis <naa3207@rit.edu>
2021-02-26 13:57:21 -0500

    adding procedural BG to balance game

---
+++ KiKi's Game/Assets/proceduralBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proceduralBackground : MonoBehaviour
+{
+    public GameObject[] clouds;
+    public GameObject[] birds;
+
+    float birdBuffer = 2f; //1s-4s
+    float cloudBuffer = 3f; //1s-4s
+
+    float yLimit = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(birdBuffer <= 0)
+        {
+            Instantiate(birds[Random.Range(0, birds.Length)], ).GetComponent<BGObj>().speed = Random.Range(20f, 30f);
+            birdBuffer = Random.Range(2f, 3f,);
+        }
+    }
+}
