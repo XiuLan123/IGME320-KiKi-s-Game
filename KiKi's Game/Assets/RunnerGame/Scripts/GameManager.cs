f3e79c074a6c5c6a8008f48bb7c9e65f60baf318
E Smith <brs9947@rit.edu>
2021-02-28 02:05:38 -0500

    fixing thing where it still had the ugly version of Runner minigame

---
+++ KiKi's Game/Assets/RunnerGame/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RunnerGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager m_instance;
+        public int score = 0;
+        public Text scoreText;
+        [SerializeField]
+        private GameObject road_Prefab;
+        public static GameObject roadPrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_instance = this;
+            roadPrefab = road_Prefab;
+            StartCoroutine(IncreaseScore());
+            StartCoroutine(WearDownBike());
+        }
+
+        public void GameOver()
+        {
+            StopAllCoroutines();
+            GameNavigation.GoToMainMenu();
+        }
+
+        public IEnumerator IncreaseScore()
+        {
+            yield return new WaitForSeconds(1f);
+            AddScore(2);
+            StartCoroutine(IncreaseScore());
+        }
+        public void AddScore(int toAdd)
+        {
+            score += toAdd;
+            scoreText.text = $"{score} pts.";
+        }
+        public IEnumerator WearDownBike()
+        {
+            yield return new WaitForSeconds(0.1f);
+            Player.TakeDamage(0.1f);
+            StartCoroutine(WearDownBike());
+        }
+    }
+}
---
+++ KiKi's Game/Assets/RunnerGame/Scripts/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(new Vector3(0.0f, 360.0f * 1.25f * Time.deltaTime, 0.0f));
+    }
+}
