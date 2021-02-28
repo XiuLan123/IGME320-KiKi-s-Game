4328e5e7e72aac8fa3f2d55852ac7d56d8ce2a87
John Zhang <1724118498@qq.com>
2021-03-01 02:01:22 +0800

    push

---
+++ KiKi's Game/Assets/Scripts/AlchemyScripts/NextBttn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextBttn : MonoBehaviour
+{
+    public GameObject stageOne;
+    public GameObject stageTwo;
+    public void Click()
+    {
+        stageOne.SetActive(false);
+        stageTwo.SetActive(true);
+    }
+}
